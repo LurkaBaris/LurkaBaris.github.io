3207567a7be0d5982451ffde0def1f99523f1b70
Ivan Puzyrev <lurkabaris@yandex.ru>
2020-09-02 14:17:59 +0400

    homepage

---
+++ precache-manifest.c3476324500c45cb76e43fbf642c07cb.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b13497c97cfcb0f282bcc62c85525f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6447ec9d1a7618e4053",
+    "url": "/static/css/main.78596f84.chunk.css"
+  },
+  {
+    "revision": "3aacce3970e5105b8077",
+    "url": "/static/js/2.e903847b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e903847b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6447ec9d1a7618e4053",
+    "url": "/static/js/main.4337765d.chunk.js"
+  },
+  {
+    "revision": "14350f566c9081e11e6b",
+    "url": "/static/js/runtime-main.b8455ab4.js"
+  },
+  {
+    "revision": "69c703b21776155b4bf13adbbb72e5a5",
+    "url": "/static/media/Montserrat-Bold.69c703b2.ttf"
+  },
+  {
+    "revision": "f8fe32d3f0e1dca05f9eece295173bd5",
+    "url": "/static/media/Montserrat-Light.f8fe32d3.ttf"
+  },
+  {
+    "revision": "f7c2cad2a5287ca6894cc1eb0080d855",
+    "url": "/static/media/Montserrat-Medium.f7c2cad2.ttf"
+  },
+  {
+    "revision": "5906fb82e31864c9b531898bb3c97d98",
+    "url": "/static/media/Montserrat-Regular.5906fb82.ttf"
+  },
+  {
+    "revision": "1bf71be111189e76987a4bb9b3115cb7",
+    "url": "/static/media/OpenSans-Light.1bf71be1.ttf"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "/static/media/OpenSans-Regular.629a55a7.ttf"
+  },
+  {
+    "revision": "7cdeaecc8b3a3def9e6c7b5728ead14a",
+    "url": "/static/media/apple.7cdeaecc.svg"
+  },
+  {
+    "revision": "3d1e619ceadb560b9e943152ebb0455b",
+    "url": "/static/media/arrow.3d1e619c.svg"
+  },
+  {
+    "revision": "611b06d62a5c95cc60f8cef89cc740fd",
+    "url": "/static/media/google.611b06d6.svg"
+  },
+  {
+    "revision": "d4f79b067bb087742741f9f6b32ae37d",
+    "url": "/static/media/item1.d4f79b06.svg"
+  },
+  {
+    "revision": "8ceba3940769a470af535b73f193765b",
+    "url": "/static/media/item10.8ceba394.png"
+  },
+  {
+    "revision": "027b6e2ee192aa0d7826edf0336f6d1a",
+    "url": "/static/media/item2.027b6e2e.png"
+  },
+  {
+    "revision": "e17115ff85fe90312742b568216f0aed",
+    "url": "/static/media/item2.e17115ff.svg"
+  },
+  {
+    "revision": "3191f04a554c8a3eccb3af598c853725",
+    "url": "/static/media/item3.3191f04a.png"
+  },
+  {
+    "revision": "d181ae58ef109a042c11f5083154e90a",
+    "url": "/static/media/item3.d181ae58.svg"
+  },
+  {
+    "revision": "1aaefa3976a6b42e795826bab8cc6129",
+    "url": "/static/media/item4.1aaefa39.png"
+  },
+  {
+    "revision": "375ac5843e92ad8df41ec2b21e636973",
+    "url": "/static/media/item4.375ac584.svg"
+  },
+  {
+    "revision": "06ee8ef8835e02df71657dc9fa15d8bc",
+    "url": "/static/media/item5.06ee8ef8.svg"
+  },
+  {
+    "revision": "07ecf2b008c565d220982b158d067198",
+    "url": "/static/media/item5.07ecf2b0.png"
+  },
+  {
+    "revision": "038dfdc3d23243a47170312a2e005664",
+    "url": "/static/media/item6.038dfdc3.png"
+  },
+  {
+    "revision": "57915e61e982e125a99f016cb63395dd",
+    "url": "/static/media/item6.57915e61.svg"
+  },
+  {
+    "revision": "c8709852f117939bf57f9c746bebb57f",
+    "url": "/static/media/item7.c8709852.png"
+  },
+  {
+    "revision": "d79c33ceadb93fe8fa856ed837120948",
+    "url": "/static/media/lc.d79c33ce.svg"
+  },
+  {
+    "revision": "d77218a1876b7b0f593b12ffe84d735b",
+    "url": "/static/media/letter.d77218a1.png"
+  },
+  {
+    "revision": "f25c619fe96356f4535afe88a257ff5b",
+    "url": "/static/media/loupe.f25c619f.svg"
+  },
+  {
+    "revision": "059b461304ec44a6e545cff1ea8af0c2",
+    "url": "/static/media/servicesItem1.059b4613.jpg"
+  },
+  {
+    "revision": "d986a19fb4c74bb8a126fdf36e5263e1",
+    "url": "/static/media/servicesItem2.d986a19f.jpg"
+  },
+  {
+    "revision": "7969ede5fa1120aae4b075fb64ae6795",
+    "url": "/static/media/servicesItem3.7969ede5.jpg"
+  },
+  {
+    "revision": "4aeb082e5e0b58582dab00c3e20897e9",
+    "url": "/static/media/servicesItem4.4aeb082e.jpg"
+  }
+]);
